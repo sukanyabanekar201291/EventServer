457386d41063e2f919f05076cd3126b6d46ef80d
Sukanya Banekar <sukanyabanekar201291@gmail.com>
2017-06-12 14:24:01 +0530

    commit

--- engine-complementory/src/main/scala/EngineConfig.scala
+++
@@ -1,9 +0,0 @@
-object EngineConfig {
-
-  val engineId = sys.env.getOrElse("ENGINE_ID", "default")
-  val engineVersion = sys.env.getOrElse("ENGINE_VERSION", "default")
-  val engineVariant = "engine.json"
-  val engineVariantId = "default"
-  println("engineId----"+engineId)
-}
-
